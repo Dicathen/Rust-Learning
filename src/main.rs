fc45815bf39325543e600861fcd2305086a1702f
Dicathen <hoalex219@gmail.com>
2022-05-20 07:08:49 -0500

    Removed .idea files from repo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#![allow(non_snake_case)]
+struct Example
+{
+    a_integer: i32
+}
+
+impl Example
+{
+    fn new(tempInt: i32) -> Self
+    {
+        Example
+        {
+            a_integer: tempInt
+        }
+    }
+    fn printString(&self)
+    {
+        println!("{}",self.a_integer)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let exampleData = Example::new(32);
+    exampleData.printString();
 }
