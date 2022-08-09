11ca3cfddf0ce09af2641069571ea16d2e1062e5
Alex H <hoalex219@gmail.com>
2022-08-09 10:54:15 +0000

    crate attribute changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![allow(non_snake_case)]
+#![allow(dead_code)]
 use crate::Entities::Print;
 mod Entities;
 
 fn main() {
     let driver1 = Entities::Driver::new(1, String::from("John"), String::from("4"), false, 4.5, true, true, String::from("None"), Entities::Type::Compact);
     driver1.PrintInfo();
-
 }
